0df7e4a199ee4b74cee4c50022d09ea987e2521b
Jefferson Souza Rodrigues <jeffersoncacula@gmail.com>
2021-08-11 14:05:26 -0400

    Fazendo funcionar

--- appFutdiBairro/src/stacks/MainStack.js
+++ appFutdiBairro/src/stacks/MainStack.js
@@ -7,13 +7,10 @@ import SingUp from '../screens/SingUp';
 
 const Stack = createStackNavigator();
 
-
-
-
-export default () => {
+export default () => (
   <Stack.Navigator>
     <Stack.Screen name="Preload" component={Preload} />
     <Stack.Screen name="SingIn" component={SingIn} />
     <Stack.Screen name="SingUp" component={SingUp} />
-  </Stack.Navigator>;
-};
+  </Stack.Navigator>
+);
