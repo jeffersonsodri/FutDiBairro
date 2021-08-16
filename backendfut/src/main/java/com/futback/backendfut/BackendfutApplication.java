57873b96772831add2ccd94046fb0016ac2845a0
Jefferson Souza Rodrigues <jeffersoncacula@gmail.com>
2021-08-16 12:13:21 -0400

    Criação do projeto SpringBoot com mongoDB

---
+++ backendfut/src/main/java/com/futback/backendfut/BackendfutApplication.java
@@ -0,0 +1,13 @@
+package com.futback.backendfut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendfutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendfutApplication.class, args);
+	}
+
+}
---
+++ backendfut/src/test/java/com/futback/backendfut/BackendfutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futback.backendfut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendfutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
